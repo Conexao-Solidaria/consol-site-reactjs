b6c506dbe49673ed97189dafb058270b44e0b1e8
rafael_scheneider <rafael.ranaudo@sptech.school>
2024-05-26 17:57:51 -0300

    feat(site-produto): adicionando estrutura base do site do produto

---
+++ site-produto/src/index.js
@@ -0,0 +1,17 @@
+import Site from './pages/Site';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ToastContainer } from 'react-toastify';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+
+import 'react-toastify/dist/ReactToastify.css'
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<Site />} />
+    </Routes>
+    <ToastContainer></ToastContainer>
+  </BrowserRouter>
+);
