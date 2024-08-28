8056cc35c9fccac690e0d885acfb261307328678
educamargo05 <eduardo.seba@sptech.school>
2024-08-27 21:33:16 -0300

    feat(): acrescentando novas partes da tela

--- site-produto/src/pages/pesquisaDoacoes/PesquisaDoacoes.jsx
+++ site-produto/src/pages/pesquisaDoacoes/PesquisaDoacoes.jsx
@@ -1,16 +1,13 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
 import NavBar from "../../components/navabar/NavBar";
 import Head from "../../components/head/Head";
 import style from "./PesquisaDoacoes.module.css";
 import iconCrianca from "../../utils/assets/icon_Crianca.png";
 import iconDoacoes from "../../utils/assets/icons_doacoes_azul.png";
-import iconLupa from "../../utils/assets/icon_lupa.png"
+import iconLupa from "../../utils/assets/icon_lupa.png";
 import iconFiltro from "../../utils/assets/icon_filtro.png"
 
-
-const pesquisaDoacoes = ({ onSearch }) => {
+const PesquisaDoacoes = ({ onSearch }) => {
     const [query, setQuery] = useState('');
 
     const handleInputChange = (event) => {
@@ -36,22 +33,30 @@ const pesquisaDoacoes = ({ onSearch }) => {
                                 <input type="text"
                                     placeholder='Pesquisar doação'
                                     value={query}
-                                    onChange={handleInputChange} 
-                                    />
-                                    <button onClick={handleSearch}><img src={iconLupa} alt="icone de pesquisar" style={{with: "20px", height:"20px"}}/></button>
+                                    onChange={handleInputChange}
+                                />
+                                <button onClick={handleSearch}><img src={iconLupa} alt="icone de pesquisar" style={{ width: "20px", height: "20px" }} /></button>
                             </div>
-
-
                         </div>
 
-                        <div className={style.containerListas}>
-                            <p>Filtrar por: </p>
-                            <button 
-                            // onClick={}
-                            ><img src={iconFiltro} alt="icone de filtro" /></button>
+                        <div className={style.containerListaDoacoes}>
+                            <div className={style.containerFiltros}>
+                                <p>Filtrar por: </p>
+                                <button
+                                // onClick={}
+                                ><img src={iconFiltro} alt="icone de filtro" /></button>
+                                <p>Filtros</p>
+                                <button
+                                // onClick={}
+                                >data</button>
+                                <button
+                                // onClick={}
+                                >hora</button>
+                            </div>
+                            
                             <div className={style.line}></div>
 
-                            <div className={style.containerListaDoacoes}>
+                            <div className={style.containerDoacao}>
                                 <img src={iconDoacoes} alt='Icone de Doações'></img>
                                 <div className={style.containerIdDoBanco}>
                                     <p>ID DO BANCO</p>
@@ -61,62 +66,171 @@ const pesquisaDoacoes = ({ onSearch }) => {
                                     <div className={style.containerInformacoes}>
                                         <div className={style.hora}></div>
                                         <p>00h00</p>
+                                        <div className={style.verticalLine}></div>
                                         <div className={style.data}>
-                                        <p> xx/xx/xxxx</p>
+                                            <p> xx/xx/xxxx</p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
 
-                            <div className={style.containerListaDoacoes}>
+                            <div className={style.containerDoacao}>
                                 <img src={iconDoacoes} alt='Icone de Doações'></img>
-                                <div className={style.containerTipoDoacao}>
-                                    <p>Doação</p>
+                                <div className={style.containerIdDoBanco}>
+                                    <p>ID DO BANCO</p>
                                     <p className={style.Categoria}>Categoria</p>
                                 </div>
                                 <div >
                                     <div className={style.containerInformacoes}>
-                                        <p className={style.paragrafo}> Nome Sobrenome</p>
+                                        <div className={style.hora}></div>
+                                        <p>00h00</p>
                                         <div className={style.verticalLine}></div>
-                                        <p> xx/xx/xxxx</p>
+                                        <div className={style.data}>
+                                            <p> xx/xx/xxxx</p>
+                                        </div>
                                     </div>
                                 </div>
-
                             </div>
-                            <div className={style.containerListaDoacoes}>
+
+                            <div className={style.containerDoacao}>
                                 <img src={iconDoacoes} alt='Icone de Doações'></img>
-                                <div className={style.containerTipoDoacao}>
-                                    <p>Doação</p>
+                                <div className={style.containerIdDoBanco}>
+                                    <p>ID DO BANCO</p>
                                     <p className={style.Categoria}>Categoria</p>
                                 </div>
                                 <div >
                                     <div className={style.containerInformacoes}>
-                                        <p className={style.paragrafo}> Nome Sobrenome</p>
+                                        <div className={style.hora}></div>
+                                        <p>00h00</p>
                                         <div className={style.verticalLine}></div>
-                                        <p> xx/xx/xxxx</p>
+                                        <div className={style.data}>
+                                            <p> xx/xx/xxxx</p>
+                                        </div>
                                     </div>
                                 </div>
-
                             </div>
-                            <div className={style.containerListaDoacoes}>
+
+
+                            <div className={style.containerDoacao}>
                                 <img src={iconDoacoes} alt='Icone de Doações'></img>
-                                <div className={style.containerTipoDoacao}>
-                                    <p>Doação</p>
+                                <div className={style.containerIdDoBanco}>
+                                    <p>ID DO BANCO</p>
                                     <p className={style.Categoria}>Categoria</p>
                                 </div>
                                 <div >
                                     <div className={style.containerInformacoes}>
-                                        <p className={style.paragrafo}> Nome Sobrenome</p>
+                                        <div className={style.hora}></div>
+                                        <p>00h00</p>
                                         <div className={style.verticalLine}></div>
-                                        <p> xx/xx/xxxx</p>
+                                        <div className={style.data}>
+                                            <p> xx/xx/xxxx</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div className={style.containerDoacao}>
+                                <img src={iconDoacoes} alt='Icone de Doações'></img>
+                                <div className={style.containerIdDoBanco}>
+                                    <p>ID DO BANCO</p>
+                                    <p className={style.Categoria}>Categoria</p>
+                                </div>
+                                <div >
+                                    <div className={style.containerInformacoes}>
+                                        <div className={style.hora}></div>
+                                        <p>00h00</p>
+                                        <div className={style.verticalLine}></div>
+                                        <div className={style.data}>
+                                            <p> xx/xx/xxxx</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+
+                            <div className={style.containerDoacao}>
+                                <img src={iconDoacoes} alt='Icone de Doações'></img>
+                                <div className={style.containerIdDoBanco}>
+                                    <p>ID DO BANCO</p>
+                                    <p className={style.Categoria}>Categoria</p>
+                                </div>
+                                <div >
+                                    <div className={style.containerInformacoes}>
+                                        <div className={style.hora}></div>
+                                        <p>00h00</p>
+                                        <div className={style.verticalLine}></div>
+                                        <div className={style.data}>
+                                            <p> xx/xx/xxxx</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+
+                            <div className={style.containerDoacao}>
+                                <img src={iconDoacoes} alt='Icone de Doações'></img>
+                                <div className={style.containerIdDoBanco}>
+                                    <p>ID DO BANCO</p>
+                                    <p className={style.Categoria}>Categoria</p>
+                                </div>
+                                <div >
+                                    <div className={style.containerInformacoes}>
+                                        <div className={style.hora}></div>
+                                        <p>00h00</p>
+                                        <div className={style.verticalLine}></div>
+                                        <div className={style.data}>
+                                            <p> xx/xx/xxxx</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div className={style.containerDoacao}>
+                                <img src={iconDoacoes} alt='Icone de Doações'></img>
+                                <div className={style.containerIdDoBanco}>
+                                    <p>ID DO BANCO</p>
+                                    <p className={style.Categoria}>Categoria</p>
+                                </div>
+                                <div >
+                                    <div className={style.containerInformacoes}>
+                                        <div className={style.hora}></div>
+                                        <p>00h00</p>
+                                        <div className={style.verticalLine}></div>
+                                        <div className={style.data}>
+                                            <p> xx/xx/xxxx</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            
+
+                            <div className={style.containerDoacao}>
+                                <img src={iconDoacoes} alt='Icone de Doações'></img>
+                                <div className={style.containerIdDoBanco}>
+                                    <p>ID DO BANCO</p>
+                                    <p className={style.Categoria}>Categoria</p>
+                                </div>
+                                <div >
+                                    <div className={style.containerInformacoes}>
+                                        <div className={style.hora}></div>
+                                        <p>00h00</p>
+                                        <div className={style.verticalLine}></div>
+                                        <div className={style.data}>
+                                            <p> xx/xx/xxxx</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-
                     </div>
                 </div>
             </div >
         </>
     )
 }
+
+export default PesquisaDoacoes;
--- site-produto/src/routes.jsx
+++ site-produto/src/routes.jsx
@@ -4,6 +4,7 @@ import { ToastContainer } from "react-toastify";
 import Cadastro from "./pages/cadastro/Cadastro";
 import Dashboard from "./pages/dashboard/Dashboard";
 import Login from "./pages/login/Login";
+import PesquisaDoacoes from "./pages/pesquisaDoacoes/PesquisaDoacoes"
 
 const Rotas = () => {
     return (
@@ -13,6 +14,7 @@ const Rotas = () => {
                     <Route path='/login' element={<Login />} />
                     <Route path='/cadastro' element={<Cadastro />} />
                     <Route path='/dashboard' element={<Dashboard />} />
+                    <Route path='/pesquisaDoacoes' element={<PesquisaDoacoes />} />
 
                 </Routes>
                 <ToastContainer></ToastContainer>
