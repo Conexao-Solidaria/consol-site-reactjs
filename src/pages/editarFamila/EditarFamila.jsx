49dd741b19c2c4257f2fb37fad4c29b40cd1c568
educamargo05 <eduardo.seba@sptech.school>
2024-08-31 22:50:26 -0300

    feat() inicio editarFamilia

---
+++ src/pages/editarFamila/EditarFamila.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from "../../components/navBar/navBar";
+import Head from "../../components/head/Head";
+import style from "./EditarFamilia.module.css";
+
+const EditarFamila = () => {
+    return (
+        <>
+            
+        </>
+    );
+};
+export default EditarFamila;
--- src/routes.jsx
+++ src/routes.jsx
@@ -9,6 +9,7 @@ import Historico from "./pages/historico/Historico";
 import Acessos from "./pages/acessos/Acessos";
 import Doacoes from "./pages/doacoes/Doacoes";
 import Donatarios from "./pages/donatarios/Donatarios";
+import EditarFamilia from "./pages/editarFamila/EditarFamila";
 
 
 const Rotas = () => {
@@ -18,7 +19,7 @@ const Rotas = () => {
                 <Routes>
                     <Route path='/' element={<Login />} />
                     <Route path='/cadastro' element={<Cadastro />} />
-                    <Route path='/teste' element={<Teste />} />
+                    <Route path='/editarFamila' element={<EditarFamilia />} />
                     <Route path='/home' element={<Dashboard />} />
                     <Route path='/historico' element={<Historico />} />
                     <Route path='/acessos' element={<Acessos />} />
