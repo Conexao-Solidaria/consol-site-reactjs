dea7a2df67ac81704634bec394469d1b00766574
educamargo05 <eduardo.seba@sptech.school>
2024-09-02 21:35:50 -0300

    feat() continucao editarFamilia

--- src/pages/editarFamila/EditarFamila.jsx
+++ src/pages/editarFamila/EditarFamila.jsx
@@ -11,7 +11,38 @@ const EditarFamila = () => {
                 <div className={style.containerHead}>
                     <Head />
 
+                    <div className={style.containerConteudo}>
+                        <div className={style.tituloPagina}>
+                            <p>Editar Família</p>
+                        </div>
+                        <div className={style.line}>‎‎‎‎‎‎‎‎ㅤ</div>
 
+                        <div className={style.containerInputImg}>
+                            <div className={style.camposTexto}>
+                                <p>Nome:</p>
+                                <input type="text"
+                                    placeholder='Nome' />
+                                <p>CEP:</p>
+                                <input type="text"
+                                    placeholder='CEP' />
+                                <p>Número da Casa:</p>
+                                <input type="text"
+                                    placeholder='Número' />
+                                <p>Renda:</p>
+                                <input type="text"
+                                    placeholder='Selecione' />
+                                    <button>Atualizar</button>
+                            </div>
+
+                            <div>
+                                <img src="" alt="foto familia feliz" />
+                            </div>
+
+                        </div>
+
+
+
+                    </div>
 
                 </div>
             </div>
