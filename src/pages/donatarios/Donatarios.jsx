f6398807b545108f26c7822d78fca39dca9591b9
Rafaqwert <rafael.rrodrigues@sptech.school>
2024-09-05 22:51:23 -0300

    feat(donatarios): adiciona navbar e head para a pagina

--- src/pages/donatarios/Donatarios.jsx
+++ src/pages/donatarios/Donatarios.jsx
@@ -1,8 +1,26 @@
-import React, { useState } from "react";
-import styles from "./Donatarios.module.css";
+import NavBar from "../../components/navBar/NavBar";
+import Head from "../../components/head/Head";
+import style from "./Donatarios.module.css";
 
 function Donatarios() {
-  return <></>;
+  return <>
+    <div className={style.container}>
+      <NavBar />
+      <div className={style.containerGeral}>
+        <div className={style.containerHead}>
+          <Head />
+        </div>
+        <div className={style.containerConteudo}>
+          <div className={style.containerPesquisa}>
+
+          </div>
+          <div className={style.containerLista}>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </>;
 }
 
 export default Donatarios;
