666b75e303e8071cc402b8b044bf50ca91e0da80
Rafaqwert <rafael.rrodrigues@sptech.school>
2024-09-08 14:50:34 -0300

    feat: adiciona conteiner de pesquisa

--- src/pages/donatarios/Donatarios.jsx
+++ src/pages/donatarios/Donatarios.jsx
@@ -5,14 +5,17 @@ import style from "./Donatarios.module.css";
 function Donatarios() {
   return <>
     <div className={style.container}>
-      <NavBar />
+      <div className={style.navbarContainer}>
+        <NavBar />
+      </div>
       <div className={style.containerGeral}>
         <div className={style.containerHead}>
           <Head />
         </div>
         <div className={style.containerConteudo}>
           <div className={style.containerPesquisa}>
-
+            <h2>Pesquisar Donatário:</h2>
+            <input type="text" placeholder="Pesquisar Donátario"/>
           </div>
           <div className={style.containerLista}>
 
