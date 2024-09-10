cb1b52d08864ae6fcaa55d71cf152192ae28940b
educamargo05 <eduardo.seba@sptech.school>
2024-09-09 21:32:04 -0300

    feat(): continuação da editarDonatario

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import Rotas from "./routes";
+import React from 'react';
+import EditarDonatario from './pages/editarDonatario/EditarDonatario';
 
 
 const App = () => {
+  const options = ['Solteiro', 'Casado', 'União Estável', 'Viúvo'];
+
+  const handleSelect = (value) => {
+    console.log('Opção selecionada:', value);
+  };
+
+  return (
+    <div>
+      <EditarDonatario options={options} onSelect={handleSelect} />
+    </div>
+  );
+
   return <Rotas />;
 };
 
--- src/pages/editarDonatario/EditarDonatario.jsx
+++ src/pages/editarDonatario/EditarDonatario.jsx
@@ -1,53 +1,81 @@
-import React from 'react';
-import SideBar from "../../components/sideBar/sideBar";
+import React, { useState } from 'react';
+import NavBar from "../../components/navBar/NavBar";
 import Head from "../../components/head/Head";
 import style from "./EditarDonatario.module.css";
 import FotoFamilia from "../../utils/assets/foto-familia.png";
 
-const EditarDonatario = () => {
+const EditarDonatario = ({ options, onSelect }) => {
+
+    const [selectedOption, setSelectedOption] = useState('');
+
+    const handleChange = (event) => {
+        setSelectedOption(event.target.value);
+        onSelect(event.target.value);
+    };
+
     return (
         <>
             <div className={style.container}>
-                <SideBar />
+                <NavBar />
                 <div className={style.containerHead}>
                     <Head />
 
                     <div className={style.containerConteudo}>
                         <div className={style.tituloPagina}>
-                            <p>Editar Família</p>
+                            <p>Editar Donatário</p>
                         </div>
                         <div className={style.line}>‎‎‎‎‎‎‎‎ㅤ</div>
 
                         <div className={style.containerFormularioImg}>
 
                             <div className={style.formulario}>
-                                <div className={style.nome}>
-                                    <p>Nome:</p>
-                                    <input type="text"
-                                        placeholder='Nome' />
-                                </div>
 
-                                <div className={style.cep}>
-                                    <p>CEP:</p>
-                                    <input type="text"
-                                        placeholder='CEP' />
+                                <div className={style.formulario1}>
+
+                                    <div className={style.nome}>
+                                        <p>Nome Completo:</p>
+                                        <input type="text"
+                                            placeholder='Nome Completo' />
+                                    </div>
+
                                 </div>
 
                                 <div className={style.formulario2}>
-                                    <div className={style.numeroCasa}>
-                                        <p>Número da Casa:</p>
+                                    <div className={style.rg}>
+                                        <p>RG:</p>
                                         <input type="text"
-                                            placeholder='Número' />
+                                            placeholder='XX.XXX.XXX-XX' />
                                     </div>
 
-                                    <div className={style.renda}>
-                                        <p>Renda:</p>
+                                    <div className={style.cpf}>
+                                        <p>CPF:</p>
                                         <input type="text"
-                                            placeholder='Renda' />
+                                            placeholder='XXX.XXX.XXX-XX' />
                                     </div>
 
                                 </div>
-                                
+
+                                <div className={style.formulario3}>
+                                    <div className={style.dataNasc}>
+                                        <p>Data de Nascimento:</p>
+                                        <input type="text"
+                                            placeholder='DD/MM/YYYY' />
+                                    </div>
+
+                                    <div className={style.estadoCivil}>
+                                        <p>Estado Civil:</p>
+                                        <select name={selectedOption} onChange={handleChange}>
+                                            <option value="" disabled>Selecione uma opção</option>
+                                            {options.map((option, index) => (
+                                                <option key={index} value={option}>
+                                                    {option}
+                                                </option>
+                                            ))}
+                                        </select>
+                                    </div>
+
+                                </div>
+
                                 <div className={style.botaoAtualizar}>
                                     <button>Atualizar</button>
                                 </div>
