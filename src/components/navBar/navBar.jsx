48e354abd588c162234e69d8ab0f59ccf0835a4e
rafael_scheneider <rafael.ranaudo@sptech.school>
2024-09-12 19:46:54 -0300

    fix(): NavBAr

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,5 +1,3 @@
-import { Link, Navigate } from 'react-router-dom';
-import { useNavigate } from "react-router-dom";
 import React, { useState } from "react";
 import styles from "./NavBar.module.css";
 import iconIgreja from "../../utils/assets/iconIgreja.png";
@@ -9,97 +7,101 @@ import iconDoacoes from "../../utils/assets/icon_doacoes.png";
 import fechadura from "../../utils/assets/fechadura.png";
 import iconPerfil from "../../utils/assets/icon_perfil.png";
 
-
-
 const NavBar = () => {
-    const navigate = useNavigate();
+  const [showText, setShowText] = useState(false);
 
-    const home = () => {
-        navigate("/");
-    }
+  const toggleText = () => {
+    setShowText(!showText);
+  };
 
-    const historico = () => {
-        navigate("/historico");
-    }
+  const [isOpen, setIsOpen] = useState(false);
 
-    const doacoes = () => {
-        navigate("/doacoes")
-    }
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
-    const acessos = () => {
-        navigate("/acessos")
-    }
+  return (
+    <>
+      <div className={styles.teste}></div>
+      <div
+        className={`${styles.container} ${showText ? styles.expandedContainer : ""}`}
+      >
+        <img
+          src={iconIgreja}
+          className={styles.iconIgreja}
+          alt="Icone de uma igreja"
+        />
 
+        <div className={styles.line}></div>
 
+        <a href="#">
+          {" "}
+          <img src={iconHome} alt="Icone de uma casa" />{" "}
+          <p style={{ display: showText ? "block" : "none" }}> Início </p>{" "}
+        </a>
+        <br />
+        <a className={styles.ativado} href="#">
+          {" "}
+          <img src={iconHistorico} alt="Icone de histórico" />{" "}
+          <p style={{ display: showText ? "block" : "none" }}> Histórico </p>{" "}
+        </a>
+        <br />
+        <a href="#">
+          {" "}
+          <img src={iconDoacoes} alt="Icone de doações" />{" "}
+          <p style={{ display: showText ? "block" : "none" }}> Doações </p>{" "}
+        </a>
+        <br />
+        <a href="#">
+          {" "}
+          <img src={iconPerfil} alt="Icone de perfil" />{" "}
+          <p style={{ display: showText ? "block" : "none" }}> Donatários</p>{" "}
+        </a>
+        <br />
+        <a href="#">
+          {" "}
+          <img src={fechadura} alt="Icone de acessos" />{" "}
+          <p style={{ display: showText ? "block" : "none" }}> Acessos</p>{" "}
+        </a>
 
-    const [showText, setShowText] = useState(false);
+        <div className={styles.containerBar} onClick={toggleText}>
+          <div className={styles.bar}></div>
+          <div className={styles.bar}></div>
+          <div className={styles.bar}></div>
+        </div>
+      </div>
 
-    const toggleText = () => {
-        setShowText(!showText);
-    };
+      <div className={styles.menuMobile}>
+        <button onClick={toggleMenu}>
+          <div className={styles.containerBar} onClick={toggleText}>
+            <div className={styles.bar}></div>
+            <div className={styles.bar}></div>
+            <div className={styles.bar}></div>
+          </div>
+        </button>
 
-    const [isOpen, setIsOpen] = useState(false);
-
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
-    };
-
-    return (
-        <>
-            <div className={styles.teste}></div>
-            <div className={`${styles.container} ${showText ? styles.expandedContainer : ''}`}>
-                <img src={iconIgreja} className={styles.iconIgreja} alt='Icone de uma igreja' />
-
-                <div className={styles.line}></div>
-
-                <a onClick={home}> <img src={iconHome} alt='Icone de uma casa' />
-                    <p style={{ display: showText ? 'block' : 'none' }}> Início    </p>
-                </a>
-                <br />
-                <a onClick={historico}> <img src={iconHistorico} alt='Icone de histórico' />
-                    <p style={{ display: showText ? 'block' : 'none' }}> Histórico </p>
-                </a>
-                <br />
-                <a onClick={doacoes} className={styles.ativado}> <img src={iconDoacoes} alt='Icone de doações' />
-                    <p style={{ display: showText ? 'block' : 'none' }}> Doações   </p> </a>
-                <br />
-                <a> <img src={iconPerfil} alt='Icone de perfil' />
-                    <p style={{ display: showText ? 'block' : 'none' }}> Donatários</p>
-                </a>
-                <br />
-                <a onClick={acessos}> <img src={fechadura} alt='Icone de acessos' />
-                    <p style={{ display: showText ? 'block' : 'none' }}> Acessos</p>
-                </a>
-
-                <div className={styles.containerBar} onClick={toggleText}>
-                    <div className={styles.bar}></div>
-                    <div className={styles.bar}></div>
-                    <div className={styles.bar}></div>
-                </div>
-
-            </div>
-
-            <div className={styles.menuMobile}>
-
-                <button onClick={toggleMenu}>
-                    <div className={styles.containerBar} onClick={toggleText}>
-                        <div className={styles.bar}></div>
-                        <div className={styles.bar}></div>
-                        <div className={styles.bar}></div>
-                    </div>
-                </button>
-
-                <nav className={`${styles.menu} ${isOpen ? styles.open : ''}`}>
-                    <a href='#'> <img src={iconHome} alt='Icone de uma casa' />       <p> Início    </p> </a>
-                    <a href='#'> <img src={iconHistorico} alt='Icone de histórico' /> <p> Histórico </p> </a>
-                    <a href='#'> <img src={iconDoacoes} alt='Icone de doações' />     <p> Doações   </p> </a>
-                    <a href='#'> <img src={iconPerfil} alt='Icone de perfil' />       <p> Donatários</p> </a>
-                </nav>
-
-            </div>
-
-        </>
-    );
+        <nav className={`${styles.menu} ${isOpen ? styles.open : ""}`}>
+          <a href="#">
+            {" "}
+            <img src={iconHome} alt="Icone de uma casa" /> <p> Início </p>{" "}
+          </a>
+          <a href="#">
+            {" "}
+            <img src={iconHistorico} alt="Icone de histórico" />{" "}
+            <p> Histórico </p>{" "}
+          </a>
+          <a href="#">
+            {" "}
+            <img src={iconDoacoes} alt="Icone de doações" /> <p> Doações </p>{" "}
+          </a>
+          <a href="#">
+            {" "}
+            <img src={iconPerfil} alt="Icone de perfil" /> <p> Donatários</p>{" "}
+          </a>
+        </nav>
+      </div>
+    </>
+  );
 };
 
 export default NavBar;
