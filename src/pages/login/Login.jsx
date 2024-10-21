85f72a0b11e0c7ff2a36fb4b8ff277d16b08b73a
rafael_scheneider <rafael.ranaudo@sptech.school>
2024-10-21 18:47:52 -0300

    feat(): guardando json do usuario

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -19,6 +19,7 @@ function Login() {
     try {
       const response = await api.post("/usuarios/login", { email, senha });
 	  sessionStorage.setItem("token", response.data.token);
+	  sessionStorage.setItem("usuario", JSON.stringify(response.data));
       toast.success("Login bem-sucedido!");
       navigate("/");
     } catch (error) {
