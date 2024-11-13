4e14f6656ce0af5a3382e27875604b6b6c4d722b
gba-exe <125586278+gba-exe@users.noreply.github.com>
2024-11-13 20:32:31 -0300

    bbbbb

--- run.sh
+++ run.sh
@@ -1,36 +1,17 @@
 #!/bin/bash
 
-# Function to run the container
-run_container() {
-  echo "Running container..."
-
-  # Use an appropriate path for Windows and Linux
-  local mount_path
-  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    mount_path="$(pwd)"
-  elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
-    # Convert Windows path to Docker's expected format
-    mount_path=$(echo "$(pwd)" | sed 's/\\/\//g' | sed 's/^C:/\/c/')
-  else
-    echo "Unsupported OS"
-    exit 1
-  fi
-
-  docker run --name consol-fe -d -p 3000:3000 -v "$mount_path:/usr/app" -i consol-fe
-}
-
-# Check if detached mode is requested
 if [[ "$1" == "-d" ]]; then
   echo "Running detached..."
 
   docker ps -q -f name=consol-fe &>/dev/null
 
   if [[ $? -ne 0 ]]; then
-    run_container
+    docker run --name consol-fe -d -p 3000:3000 -v "$(pwd):/usr/app" -i consol-fe
   else
     echo "Container already exists. Restarting..."
+
     docker rm -f consol-fe
-    run_container
+    docker run --name consol-fe -d -p 3000:3000 -v "$(pwd):/usr/app" -i consol-fe
   fi
   exit 0
 fi
@@ -39,10 +20,11 @@ docker ps -q -f name=consol-fe &>/dev/null
 
 if [[ $? -ne 0 ]]; then
   echo "Container does not exist. Creating and running..."
-  run_container
+
+  docker run --name consol-fe -p 3000:3000 -v "$(pwd):/usr/app" -i consol-fe
 else
   echo "Container already exists. Restarting..."
-  docker rm -f consol-fe
-  run_container
-fi
 
+  docker rm -f consol-fe
+  docker run --name consol-fe -p 3000:3000 -v "$(pwd):/usr/app" -i consol-fe
+fi
