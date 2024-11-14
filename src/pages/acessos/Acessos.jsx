708fcb53cd58108df4ae8f2c5576acb26ba1f707
rafael_scheneider <rafael.ranaudo@sptech.school>
2024-11-14 18:20:52 -0300

    feat(): Subindo primeira parte da refatoração de acessos

--- src/pages/acessos/Acessos.jsx
+++ src/pages/acessos/Acessos.jsx
@@ -11,109 +11,178 @@ const Acessos = () => {
 
 	useEffect(() => {
 		if (sessionStorage.getItem("token") == null && sessionStorage.getItem("user") == undefined) {
-			navigate("/")
+			navigate("/");
 		}
-	})
-    const [usuarios, setUsuarios] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const yourConfig = {
-        headers: {
-            'Authorization': "Bearer " + sessionStorage.getItem("token")
-        }
-    }
+	});
 
-    const fetchUsuarios = async () => {
-        try {
-            const response = await api.get("/usuarios", yourConfig);
-            setUsuarios(response.data);
-        } catch (error) {
-            console.error("Erro ao buscar usuários:", error);
-        } finally {
-            setLoading(false);
-        }
-    };
+	const [usuarios, setUsuarios] = useState([]);
+	const [usuariosDentroSistema, setUsuariosDentroSistema] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [searchTerm, setSearchTerm] = useState("");
 
-    useEffect(() => {
-        fetchUsuarios();
-    }, []);
+	const yourConfig = {
+		headers: {
+			'Authorization': "Bearer " + sessionStorage.getItem("token"),
+		},
+	};
 
-    const handleAccept = async (id) => {
-        try {
-            const flagAceitar = {
-                flagAprovado: 1
-            }
-            await api.put(`/usuarios/atualizar-flag/${id}`, flagAceitar, yourConfig);
-            toast.success(`Acesso aceito para o usuário com ID: ${id}`);
-            setUsuarios(usuarios.filter(usuario => usuario.id !== id));
-        } catch (error) {
-            console.error("Erro ao aceitar acesso:", error);
-        }
-    };
+	const fetchUsuarios = async () => {
+		try {
+			const response = await api.get("/usuarios", yourConfig);
+			let usuariosFora = [];
+			let usuariosDentro = [];
 
-    const handleReject = async (id) => {
-        try {
-            const flagRecusar = {
-                flagAprovado: 0
-            }
-            await api.put(`/usuarios/atualizar-flag/${id}`, flagRecusar, yourConfig);
-            toast.error(`Acesso negado para o usuário com ID: ${id}`);
-            setUsuarios(usuarios.filter(usuario => usuario.id !== id));
-        } catch (error) {
-            console.error("Erro ao recusar acesso:", error);
-        }
-    };
+			response.data.map(usuario => {
+				if (usuario.idUsuario == JSON.parse(sessionStorage.getItem("usuario")).userId) {
+					// No action
+				} else if (usuario.flagAprovado === 1) {
+					usuariosDentro.push(usuario);
+				} else {
+					usuariosFora.push(usuario);
+				}
+			});
+			setUsuariosDentroSistema(usuariosDentro);
+			setUsuarios(usuariosFora);
+		} catch (error) {
+			console.error("Erro ao buscar usuários:", error);
+		} finally {
+			setLoading(false);
+		}
+	};
 
-    if (loading) {
-        return <div>Carregando...</div>;
-    }
+	useEffect(() => {
+		fetchUsuarios();
+	}, []);
 
-    return (
-        <div className={style.container}>
-            <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-            <NavBar />
-            <div className={style.main}>
-                <div className={style.containerHead}>
-                    <Head />
-                </div>
-                <div className={style.pesquisa}>
-                    <span className={style.tituloTexto}>Pesquisar Usuários:</span>
-                    <input type="text" placeholder="Pesquisar Usuário" />
-                </div>
-                <div className={style.gerenciar}>
-                    <div className={style.gerenciarTitulo}>
-                        <div className={style.titulo}>
-                            <span className={style.tituloTexto}>Controle de Acessos</span>
-                        </div>
-                        <hr className="hr" />
-                    </div>
-                    <div className={style.cartoes}>
-                        {usuarios.map((usuario, index) => (
-                            <div key={index} className={style.cartao}>
-                                <div>
-                                    <h2>{usuario.nomeUsuario}</h2>
-                                    <span>{usuario.email}</span>
-                                </div>
-                                <div>
-                                    <button
-                                        className={style.button1}
-                                        onClick={() => handleAccept(usuario.idUsuario)}
-                                    >
-                                        <i className="material-icons">arrow_forward</i> Aceitar
-                                    </button>
-                                    <button
-                                        className={style.button2}
-                                        onClick={() => handleReject(usuario.idUsuario)}
-                                    >
-                                        <i className="material-icons">close</i> Negar
-                                    </button>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+	const handleAccept = async (id) => {
+		try {
+			const flagAceitar = { flagAprovado: 1 };
+			await api.put(`/usuarios/atualizar-flag/${id}`, flagAceitar, yourConfig);
+			toast.success(`Acesso aceito para o usuário com ID: ${id}`);
+			setUsuarios(usuarios.filter(usuario => usuario.id !== id));
+		} catch (error) {
+			console.error("Erro ao aceitar acesso:", error);
+		}
+	};
+
+	const handleReject = async (id) => {
+		try {
+			const flagRecusar = { flagAprovado: 0 };
+			await api.put(`/usuarios/atualizar-flag/${id}`, flagRecusar, yourConfig);
+			toast.error(`Acesso negado para o usuário com ID: ${id}`);
+			setUsuarios(usuarios.filter(usuario => usuario.id !== id));
+		} catch (error) {
+			console.error("Erro ao recusar acesso:", error);
+		}
+	};
+
+	const handleDelete = async (id) => {
+		try {
+			await api.delete(`/usuarios/${id}`, yourConfig);
+			toast.error(`Acesso negado para o usuário com ID: ${id}`);
+			setUsuarios(usuarios.filter(usuario => usuario.id !== id));
+		} catch (error) {
+			console.error("Erro ao recusar acesso:", error);
+		}
+	};
+
+	const filteredUsuarios = usuarios.filter(usuario => 
+		usuario.nomeUsuario.toLowerCase().includes(searchTerm.toLowerCase()) ||
+		usuario.email.toLowerCase().includes(searchTerm.toLowerCase())
+	);
+
+	const filteredUsuariosDentro = usuariosDentroSistema.filter(usuario => 
+		usuario.nomeUsuario.toLowerCase().includes(searchTerm.toLowerCase()) ||
+		usuario.email.toLowerCase().includes(searchTerm.toLowerCase())
+	);
+
+	if (loading) {
+		return <div>Carregando...</div>;
+	}
+
+	return (
+		<div className={style.container}>
+			<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+			<NavBar />
+			<div className={style.main}>
+				<div className={style.containerHead}>
+					<Head />
+				</div>
+				<div className={style.pesquisa}>
+					<span className={style.tituloTexto}>Pesquisar Usuários:</span>
+					<input 
+						type="text" 
+						placeholder="Pesquisar Usuário" 
+						value={searchTerm}
+						onChange={(e) => setSearchTerm(e.target.value)}
+					/>
+				</div>
+				<div className={style.gerenciar}>
+					<div className={style.gerenciarTitulo}>
+						<div className={style.titulo}>
+							<span className={style.tituloTexto}>Controle de Acessos Pendentes</span>
+						</div>
+						<hr className="hr" />
+					</div>
+					<div className={style.cartoes}>
+						{filteredUsuarios.map((usuario, index) => (
+							<div key={index} className={style.cartao}>
+								<div>
+									<h2>{usuario.nomeUsuario}</h2>
+									<span>{usuario.email}</span>
+								</div>
+								<div className={style.buttons}>
+									<button
+										className={style.button1}
+										onClick={() => handleAccept(usuario.idUsuario)}
+									>
+										<i className="material-icons">arrow_forward</i> Aceitar
+									</button>
+									<input type="checkbox" id={usuario.idUsuario} />
+									<span>Como administrador?</span>
+									<button
+										className={style.button2}
+										onClick={() => handleDelete(usuario.idUsuario)}
+									>
+										<i className="material-icons">close</i> Negar
+									</button>
+								</div>
+							</div>
+						))}
+					</div>
+				</div>
+				<div className={style.gerenciar}>
+					<div className={style.titulo}>
+						<span className={style.tituloTexto}>Controle de Acessos Dentro do Sistema</span>
+					</div>
+					<div className={style.cartoes}>
+						{filteredUsuariosDentro.map((usuario, index) => (
+							<div key={index} className={style.cartao}>
+								<div>
+									<h2>{usuario.nomeUsuario}</h2>
+									<span>{usuario.email}</span>
+								</div>
+								<div>
+									<button
+										className={style.button1}
+										onClick={() => handleAccept(usuario.idUsuario)}
+									>
+										<i className="material-icons">arrow_forward</i> Aceitar
+									</button>
+									<button
+										className={style.button2}
+										onClick={() => handleReject(usuario.idUsuario)}
+									>
+										<i className="material-icons">close</i> Retirar acesso
+									</button>
+								</div>
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Acessos;
