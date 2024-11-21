d5accb840713ec665a4c4967bf22e54f79e3ebf4
gba-exe <125586278+gba-exe@users.noreply.github.com>
2024-11-21 20:27:33 -0300

    feat(modalDonatario): adicionando validacao de adm

--- src/components/modalDonatario/ModalDonatario.jsx
+++ src/components/modalDonatario/ModalDonatario.jsx
@@ -53,6 +53,11 @@ const ModalDonatario = ({ data, isVisible, onClose }) => {
   };
 
   async function handleDelete(id) {
+
+    if (JSON.parse(sessionStorage.getItem("usuario")).coordenador != 1) {
+      toast.error("Você não tem permissão para ver a tela de acessos")
+    }
+
     const yourConfig = {
       headers: {
         Authorization: "Bearer " + sessionStorage.getItem("token"),
@@ -183,10 +188,10 @@ const ModalDonatario = ({ data, isVisible, onClose }) => {
                         <b>
                           {data?.donatario.telefone1
                             ? (() => {
-                                const tel = data.donatario.telefone1;
-                                const formattedTel = `(${tel.slice(0, 2)}) ${tel.slice(2, 7)}-${tel.slice(7)}`;
-                                return formattedTel;
-                              })()
+                              const tel = data.donatario.telefone1;
+                              const formattedTel = `(${tel.slice(0, 2)}) ${tel.slice(2, 7)}-${tel.slice(7)}`;
+                              return formattedTel;
+                            })()
                             : "Não disponível"}
                         </b>
                       </div>
@@ -195,10 +200,10 @@ const ModalDonatario = ({ data, isVisible, onClose }) => {
                         <b>
                           {data?.donatario.telefone2
                             ? (() => {
-                                const tel = data.donatario.telefone2;
-                                const formattedTel = `(${tel.slice(0, 2)}) ${tel.slice(2, 7)}-${tel.slice(7)}`;
-                                return formattedTel;
-                              })()
+                              const tel = data.donatario.telefone2;
+                              const formattedTel = `(${tel.slice(0, 2)}) ${tel.slice(2, 7)}-${tel.slice(7)}`;
+                              return formattedTel;
+                            })()
                             : "Não disponível"}
                         </b>
                       </div>
