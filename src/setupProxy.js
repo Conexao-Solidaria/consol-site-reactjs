00270d2d75beba260d5786eef1c998858b322ceb
rafael_scheneider <rafael.ranaudo@sptech.school>
2024-12-03 10:11:16 -0300

    test/ Proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
         '/java-api',
         createProxyMiddleware({
-            target: "http://localhost:8080",
+            target: "http://10.0.0.26:8080",
             ChangeOrigin: true
         })
     );
